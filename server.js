88134b76d40e05fc852d39fd9d156d15dbb2b89c
Suvauny Campbell <76236640+suvauny@users.noreply.github.com>
2022-06-03 12:31:32 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const req = require("express/lib/request");
+
+const app = express();
+// This is your test secret API key.
+const stripe = require("stripe")('sk_test_51IRYcnGTO6GEZkLI50bVJjbIBatNqz5NIRV21bbyDVFPG23yG3kb4lbC1o1Px6DCfawa7sw5Ozk8XAGzvyhPTdmv00Tnat6qAW');
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send('Working');
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  const items = req.body.amount;
+  var total = Number(items)
+   
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+app.listen(3000, () => console.log("Node server listening on port 3000!"));
